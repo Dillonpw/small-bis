409cd60fe81b911fcb2d8afb8d78e2f035008152
dillonpw <dillonpwalsh10@gmail.com>
2024-06-02 09:40:13 -0400

    updated types

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun } from "@fortawesome/free-regular-svg-icons";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("theme") === "dark";
@@ -12,17 +12,17 @@ const ThemeToggle = () => {
     document.documentElement.classList.toggle("dark", isDarkMode);
   }, []);
 
-  const toggleTheme = () => {
+  const toggleTheme = useCallback(() => {
     const isDarkMode = !darkMode;
     setDarkMode(isDarkMode);
     localStorage.setItem("theme", isDarkMode ? "dark" : "light");
     document.documentElement.classList.toggle("dark", isDarkMode);
-  };
+  }, [darkMode]);
 
   return (
     <div className="mx-2">
       <button
-        className="text-2xl font-bold text-yellow-400 w-10 h-10 mt-2 flex justify-center items-center"
+        className="mt-2 flex h-10 w-10 items-center justify-center text-2xl font-bold text-yellow-400"
         onClick={toggleTheme}
       >
         <FontAwesomeIcon icon={darkMode ? faSun : faMoon} fixedWidth />
