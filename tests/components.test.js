5e7a479b393db4c7808a0c30297dd38392b51203
dillonpw <dillonpwalsh10@gmail.com>
2024-08-03 11:13:18 -0400

    cut testing

--- tests/components.test.js
+++
@@ -1,9 +0,0 @@
-import { experimental_AstroContainer as AstroContainer } from "astro/container";
-import { expect, test } from "vitest";
-import About, { aboutText } from "@/components/about.astro";
-
-test("About", async () => {
-  const container = await AstroContainer.create();
-  const result = await container.renderToString(About);
-  expect(result).toContain("m-10 text-left text-xl", { text: aboutText });
-});
